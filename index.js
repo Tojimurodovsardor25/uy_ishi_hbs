734ae49b43efa42c9e9f475c905f538a40c112e5
Sardor <tojimuradovsardor25@gmail.com>
2021-09-25 19:27:55 +0500

    drv_hbs___
    HBS

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extname: 'hbs'
-})
+    const hbs = exphbs.create({
+        defaultLayout: 'main',
+        extname: 'hbs'
+    })
 
 app.engine('hbs', hbs.engine)
-app.set('views engine', 'hbs')
+app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.get('/', (req, res) => {
-    res.render('index.hbs')
+    res.render('index')
 })
 
 app.get('/contact', (req, res) => {
-    res.render('contact.hbs')
+    res.render('contact')
 })
 
 app.get('/index', (req, res) => {
-    res.render('index.hbs')
+    res.render('index')
 })
 
 app.listen(port, () => {
